ce863b918f5ce1bf70d33a454ac1ec3839e2e9ea
DeepikaZote <deepikazote40@gmail.com>
2026-09-12 20:51:17 +0530

    Update MySQL connection password comment

--- dashboard/main.py
+++ dashboard/main.py
@@ -35,7 +35,7 @@ def get_db():
         host='localhost',
         port=3306,
         user='root',
-        password='',     # change this to your own MySQL password
+        password='',     
         database='caresync'
     )
 
